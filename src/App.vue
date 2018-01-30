267bd8e20c0785cb5d183f88877952ab9fdfc57e
ornellaramosv <30955391+ornellaramosv@users.noreply.github.com>
2018-01-30 12:32:58 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -23,9 +23,10 @@
     <div class="progress" v-if="loading">
       <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
     </div>
+    
     <ul v-else class="list-group">
       <li v-for="tweet in tweets" :key="tweet._id" class="list-group-item">
-        {{ tweet.content }} - {{ tweet.location }} - {{ tweet.author.firstname }} {{ tweet.author.lastname }}
+        {{ tweet.content }} - {{ tweet.location }} - {{ tweet.author.firstname }} {{ tweet.author.lastname }} - {{ tweet.author.createdAt }}
       </li>
     </ul>
   </div>
